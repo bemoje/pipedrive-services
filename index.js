ef36af81d2af4ac2868d450312099f40157d5d7d
Admin <bemoje@bemoje.net>
2021-10-04 20:10:04 +0200

    script test run readied

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const app = express();
 const Pipeprive = require('pipedrive');
 const fs = require('fs');
+const log = require('loglevel');
 
-const PORT = 1800;
+// set log level
+log.setLevel("info");
 
+// import API token
 Pipeprive.Configuration.apiToken = require('./API_TOKEN.json').token;
 
+// set express app port
+const PORT = 1800;
+
+// Pipeprive controllers
 const dealsController = Pipeprive.DealsController;
 const organizationsController = Pipeprive.OrganizationsController;
 const personsController = Pipeprive.PersonsController;
@@ -16,46 +23,66 @@ const usersController = Pipeprive.UsersController;
  * Custom Error class for API GET-request errors
  */
 class APIGetRequestError extends Error {
-	constructor(message) {
-		super(message)
-		this.name = "APIGetRequestError"
+	constructor (message) {
+		super(message);
+		this.name = "APIGetRequestError";
 	}
 }
 
 /**
  * Repeatedly invokes a pipedrive node-library HTTP GET-request function until all the data availble through pagination
  * is completed.
- * @async
  * @param {function} f - API function
  * @param {Array<any>} options - The options object with which to use as the first argument when invoking 'f'. The
  * properties, 'start' and 'limit' will be overridden.
  * @throws {APIGetRequestError} if API call is not successful
  * @returns {Promise<Array<object>>}
  */
-async function depletePagination(f, options = {}) {
+async function depletePagination (f,options = {}) {
 	try {
-		const data = []
-		let hasMoreItems = true
-		let nextPaginationStart = 0
+		const data = [];
+		let hasMoreItems = true;
+		let nextPaginationStart = 0;
 		while (hasMoreItems) {
-			options.start = nextPaginationStart
-			options.limit = 5000
-			const result = await f(options)
+			options.start = nextPaginationStart;
+			options.limit = 500;
+			const result = await f(options);
 			if (result.success !== true) {
-				throw new APIGetRequestError(f.name + ' API call not successful')
+				throw new APIGetRequestError(f.name + ' API call not successful');
 			}
-			for (let i = 0; i < result.data.length; i++) {
-				data.push(result.data[i])
+			if (result.data) {
+				for (let i = 0; i < result.data.length; i++) {
+					data.push(result.data[ i ]);
+				}
 			}
 			if (result.additional_data.pagination.more_items_in_collection) {
-				nextPaginationStart = result.additional_data.pagination.next_start
+				nextPaginationStart = result.additional_data.pagination.next_start;
 			} else {
-				hasMoreItems = false
+				hasMoreItems = false;
 			}
 		}
-		return data
+		return data;
 	} catch (e) {
-		console.error(e)
+		console.error(e);
+	}
+}
+
+/**
+* Invokes a pipedrive node-library HTTP GET-request function that does not use pagination.
+* @param {function} f - API function
+* @param {Array<any>} options - The options object with which to use as the first argument when invoking 'f'.
+* @throws {APIGetRequestError} if API call is not successful
+* @returns {Promise<Array<object>>}
+*/
+async function getNonPagination (f,options) {
+	try {
+		const result = await f(options);
+		if (result.success !== true) {
+			throw new APIGetRequestError(f.name + ' API call not successful');
+		}
+		return result.data;
+	} catch (e) {
+
 	}
 }
 
@@ -64,11 +91,11 @@ async function depletePagination(f, options = {}) {
  * @throws {APIGetRequestError} if API call is not successful
  * @returns {Promise<Array<object>>}
  */
-async function getAllDeals() {
+async function getAllDeals () {
 	try {
-		return await depletePagination(dealsController.getAllDeals)
+		return await depletePagination(dealsController.getAllDeals);
 	} catch (e) {
-		console.log(e);
+		log.error(e);
 	}
 }
 
@@ -77,21 +104,19 @@ async function getAllDeals() {
  * @throws {APIGetRequestError} if API call is not successful
  * @returns {Promise<Array<object>>}
  */
-async function getAllOpenDeals() {
+async function getAllOpenDeals () {
 	try {
-		/*
-		const data = await getAllDeals()
-		const openDeals = []
-		for (let i = 0; i < data.length; i++) {
-			if (data[i].status === 'open') {
-				openDeals.push(data[i])
+		const data = await getAllDeals();
+		const openDeals = [];
+		for (const deal of data) {
+			if (deal.status === 'open') {
+				openDeals.push(deal);
 			}
 		}
-		return openDeals
-		*/
-		return require('E:\\repos\\packages\\pipedrive-services\\temp\\allOpenDeals.json')
+		return openDeals;
+		//return require('E:\\repos\\packages\\pipedrive-services\\temp\\allOpenDeals.json');
 	} catch (e) {
-		console.log(e);
+		log.error(e);
 	}
 }
 
@@ -100,12 +125,12 @@ async function getAllOpenDeals() {
  * @throws {APIGetRequestError} if API call is not successful
  * @returns {Promise<Array<object>>}
  */
-async function getAllOrganizations() {
+async function getAllOrganizations () {
 	try {
-		// return await depletePagination(organizationsController.getAllOrganizations)
-		return require('E:\\repos\\packages\\pipedrive-services\\temp\\allOrganizations.json')
+		return await depletePagination(organizationsController.getAllOrganizations);
+		//return require('E:\\repos\\packages\\pipedrive-services\\temp\\allOrganizations.json');
 	} catch (e) {
-		console.log(e);
+		log.error(e);
 	}
 }
 
@@ -114,12 +139,12 @@ async function getAllOrganizations() {
  * @throws {APIGetRequestError} if API call is not successful
  * @returns {Promise<Array<object>>}
  */
-async function getAllPeople() {
+async function getAllPeople () {
 	try {
-		//return await depletePagination(personsController.getAllPersons)
-		return require('E:\\repos\\packages\\pipedrive-services\\temp\\allPeople.json')
+		return await depletePagination(personsController.getAllPersons);
+		//return require('E:\\repos\\packages\\pipedrive-services\\temp\\allPeople.json');
 	} catch (e) {
-		console.log(e);
+		log.error(e);
 	}
 }
 
@@ -128,19 +153,129 @@ async function getAllPeople() {
  * @throws {APIGetRequestError} if API call is not successful
  * @returns {Promise<Array<object>>}
  */
-async function getAllUsers() {
+async function getAllUsers () {
 	try {
-		/*
-		const result = await usersController.getAllUsers()
-		if (result.success !== true) {
-			throw new APIGetRequestError(f.name + ' API call not successful')
-		}
-		return result.data
-		*/
-		return require('E:\\repos\\packages\\pipedrive-services\\temp\\allUsers.json')
-	} catch (e) {
-		console.log(e);
+		return await getNonPagination(usersController.getAllUsers);
+		//return require('E:\\repos\\packages\\pipedrive-services\\temp\\allUsers.json');
 	}
+	catch (e) {
+		log.error(e);
+	}
+}
+
+/**
+ * Returns an array of all people in an organization
+ * @param {number} id - Integer ID of the organization
+ * @throws {APIGetRequestError} if API call is not successful
+ * @returns {Promise<Array<object>>}
+ */
+async function listPersonsOfAnOrganization (id) {
+	try {
+		return await depletePagination(organizationsController.listPersonsOfAnOrganization,{
+			id
+		});
+	} catch (e) {
+		log.error(e);
+	}
+}
+
+/**
+ * Returns the ID of an organization.
+ * @param {object} organization
+ * @returns {number} integer ID
+ */
+function getDealOrganizationOwnerID (deal) {
+	return deal.org_id.owner_id;
+}
+
+/**
+ * Returns the ID of a deal's owner.
+ * @param {object} deal
+ * @returns {number} integer ID
+ */
+function getDealOwnerID (deal) {
+	return deal.user_id.value;
+}
+
+/**
+ * Returns the name of a deal's owner.
+ * @param {object} deal
+ * @returns {string}
+ */
+function getDealOwnerName (deal) {
+	return deal.user_id.name;
+}
+
+/**
+ * Returns the title of a deal.
+ * @param {object} deal
+ * @returns {string}
+ */
+function getDealTitle (deal) {
+	return deal.title;
+}
+
+/**
+ * Returns the ID of an organization.
+ * @param {object} organization
+ * @returns {number} integer ID
+ */
+function getOrganizationID (organization) {
+	return organization.id;
+}
+
+/**
+ * Returns the name of an organization.
+ * @param {object} organization
+ * @returns {string}
+ */
+function getOrganizationName (organization) {
+	return organization.name;
+}
+
+/**
+ * Returns the ID of an organization.
+ * @param {object} organization
+ * @returns {number} integer ID
+ */
+function getOrganizationOwnerID (organization) {
+	return organization.owner_id.value;
+}
+
+/**
+ * Returns the name of the owner of an organization.
+ * @param {object} organization
+ * @returns {string}
+ */
+function getOrganizationOwnerName (organization) {
+	return organization.owner_id.name;
+}
+
+/**
+ * Returns the number of contact people an organization has.
+ * @param {object} organization
+ * @returns {number} integer
+ */
+function getOrganizationPeopleCount (organization) {
+	return organization.people_count;
+}
+
+/**
+ * Returns the owner ID of a contact person.
+ * @param {object} person
+ * @returns {number} integer ID
+ */
+function getPersonOwnerID (person) {
+	return person.owner_id.value;
+}
+
+/**
+ * Returns the name of a contact person.
+ * @param {object} person
+ * @returns {string}
+ */
+function getPersonName (person) {
+	return person.name;
 }
 
 /**
@@ -152,66 +287,66 @@ async function getAllUsers() {
  * organisation som Deal er tilknyttet. Der næst, trække en liste over alle Organisations, se for hver enkelt hvem som
  * er owner, og så sætte vedkommende til at være owner af alle de Contacts som er tilknyttet organisationen.
  */
-async function main() {
+async function main () {
 	try {
+		// Create memory cache
+		const users = await getAllUsers();
+		log.info('INFO: All users downloaded.');
 
-		// const user = await lib.UsersController.getCurrentUserData();
-		// console.log(user)
+		const openDeals = await getAllOpenDeals();
+		log.info('INFO: All open deals downloaded.');
 
-		const openDeals = await getAllOpenDeals()
-		const organizations = await getAllOrganizations()
-		const people = await getAllPeople()
-		const users = await getAllUsers()
+		const organizations = await getAllOrganizations();
+		log.info('INFO: All organizations downloaded.');
 
 		//fs.writeFileSync('E:\\repos\\packages\\pipedrive-services\\temp\\allOpenDeals.json', JSON.stringify(openDeals), 'utf8');
 		//fs.writeFileSync('E:\\repos\\packages\\pipedrive-services\\temp\\allOrganizations.json', JSON.stringify(organizations), 'utf8');
-		//fs.writeFileSync('E:\\repos\\packages\\pipedrive-services\\temp\\allPeople.json', JSON.stringify(people), 'utf8');
 		//fs.writeFileSync('E:\\repos\\packages\\pipedrive-services\\temp\\allUsers.json', JSON.stringify(users), 'utf8');
 
-		// console.log(openDeals[10])
-		// console.log(organizations[10])
-		// console.log(people[10])
-		// console.log(users[10])
 
-		console.log('openDeals: ' + openDeals.length)
-		console.log('organizations: ' + organizations.length)
-		console.log('people: ' + people.length)
-		console.log('users: ' + users.length)
-
-
+		// Ensure deal owners are also owners of the organizations related to the deal.
 		for (const deal of openDeals) {
-			if (deal.user_id) {
-				//console.log(deal)
+			if (!deal.org_id) {
+				log.warn('WARNING: No organization found related to open deal ' + getDealTitle(deal));
+				continue;
 			}
+			if (getDealOwnerID(deal) !== getDealOrganizationOwnerID(deal)) {
+				log.trace(`ACTION: Set deal owner (${getDealOwnerName(deal)}) to be owner of organization ${getDealOwnerName(deal)}`);
+			}
+			log.info('INFO: Finished processing deal: ' + getDealTitle(deal));
 		}
 
-		// find alle open deals, som har anden ejer end TMJ
-		console.log(openDeals.find((value, i) => {
-			if (value.user_id) {
-				return value.user_id.name !== 'Thomas Møller Jensen'
-			} else {
-				return false
+		// Ensure owner of all organizations also owns their respective contact people.
+		for (const organization of organizations) {
+			if (!organization.owner_id) {
+				log.warn('WARNING: Organization has no owner: ' + getOrganizationName(organization));
+				continue;
 			}
-		}))
-
-		// find alle kontaktpersoner, som har anden ejer end TMJ
-		console.log(people.find((value, i) => {
-			if (value.owner_id) {
-				return value.owner_id.name === 'Thomas Møller Jensen'
-			} else {
-				return false
+			if (getOrganizationPeopleCount(organization) < 1) {
+				log.info(`INFO: Organization has no contact people: ${getOrganizationName(organization)}`);
+				continue;
 			}
-		}))
-
+			const people = await listPersonsOfAnOrganization(getOrganizationID(organization));
+			for (const person of people) {
+				if (!person.owner_id) {
+					log.warn('WARNING: Contact person has no owner: ' + getPersonName(person));
+					continue;
+				}
+				if (getPersonOwnerID(person) !== getOrganizationOwnerID(organization)) {
+					log.trace(`ACTION: Set (${getOrganizationOwnerName(organization)}) to be owner of contact person ${getPersonName(person)}, ${getOrganizationName(organization)}`);
+				}
+				log.info('INFO: Finished processing contact person: ' + getPersonName(person));
+			}
+			log.info('INFO: Finished processing organization: ' + getOrganizationName(organization));
+		}
 	} catch (e) {
-		console.log(e);
+		log.error(e);
 	}
 }
 
 main().catch((e) => {
-	console.log(e)
-})
-
+	console.log(e);
+});
 
 
 /*
